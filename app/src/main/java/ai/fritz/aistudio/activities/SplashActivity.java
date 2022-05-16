7d670f63ddb2100a3ed920423d6842e8b921e3aa
Rakesh137 <chauhan.rakesh137@gmail.com>
2022-05-16 11:35:13 +0530

    Initial commit

---
+++ app/src/main/java/ai/fritz/aistudio/activities/SplashActivity.java
@@ -0,0 +1,21 @@
+package ai.fritz.aistudio.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * The splash activity is the entry point for the rest of the app.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
